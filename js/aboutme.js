738239f7e7a6e9e22440ce59d17e64ff76eb3b98
jacobl992 <jake_lewis12@hotmail.co.uk>
2023-10-19 21:48:35 +0100

    picture sizes decreased

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,7 +1,8 @@
 const mysteryBox = document.querySelector(".mystery");
 const questionMark = document.querySelector(".mystery-qmark");
 const rab = new Image();
-rab.src = "../images/rab.jpg";
+//rab.src = "../images/rabSmall.jpeg";
+rab.src = "images/rabSmall.jpeg";
 rab.classList.add("rab-img");
 
 questionMark.addEventListener("click", () => {
