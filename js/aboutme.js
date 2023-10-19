bbaba8c531ed7215a7e5afb5cf493a644819f745
jacobl992 <jake_lewis12@hotmail.co.uk>
2023-10-19 22:41:05 +0100

    added more pics to photographer section in JS

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,11 +1,33 @@
 const mysteryBox = document.querySelector(".mystery");
 const questionMark = document.querySelector(".mystery-qmark");
 const rab = new Image();
-//rab.src = "../images/rabSmall.jpeg";
-rab.src = "images/rabSmall.jpeg";
-rab.classList.add("rab-img");
+let buttonCount = 1;
+const nextButton = document.createElement("button");
+nextButton.classList.add("next-button")
+
 
 questionMark.addEventListener("click", () => {
+    rab.src = "images/rabSmall.jpeg";
+    rab.classList.add("rab-img");
+    rab.style.display = "block";
     questionMark.style.display = "none";
     mysteryBox.appendChild(rab);
+    mysteryBox.appendChild(nextButton)
+    nextButton.innerText = "Next";
 });
+//theres a problem on the second time around when the next button doesnt appear
+
+nextButton.addEventListener("click", () => {
+    buttonCount++;
+    if (buttonCount === 2) {
+        rab.src = "images/cowMedium.jpeg";
+    } else if (buttonCount === 3) {
+    rab.src = "images/sealMedium.jpeg";
+    } else {
+        rab.style.display = "none";
+        nextButton.style.display = "none";
+        questionMark.style.display = "block";
+    }
+})
+
+
